ca1c29c8247d21356608c177c5fc8a97c9f5beab
alexmldev <alexmldev@gmail.com>
2022-05-18 10:41:32 +0200

    fix component style imports

--- src/components/cart-dropdown/cart-dropdown.styles.tsx
+++ src/components/cart-dropdown/cart-dropdown.styles.tsx
@@ -4,7 +4,7 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
-} from "../button/button.styles.js";
+} from "../button/button.styles";
 
 export const CartDropDownContainer = styled.div`
   position: absolute;
